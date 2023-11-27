63ad9d3a313e50506925e99627cc3e22545b9968
SaikoChan000 <61124965+SaikoChan000@users.noreply.github.com>
2023-11-27 16:26:39 +0100

    Fixed Migration Issue and added sample data to table "items"

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Setting up
 
-docker compose up db dbmigration
+## Start Database and apply migrations
+
+docker compose up db dbmigration --build
+
+__We include --build to rebuild the migrations into the migrations container__
+
+## Run the NodeJS application
+
 nodemon index.js
--- index.js
+++ index.js
@@ -6,7 +6,86 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ extended: true }))
 
-app.get('/pets', async (req, res) => {
+//CRUD functionalities for ITEMS
+//show all items
+app.get('/items', async (req, res) => {
+    const allItemsQueryString = 'SELECT * FROM items'
+    try {
+        const result = await db.query(allItemsQueryString);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+//add new item with name, price
+app.post('/items', async (req, res) => {
+
+});
+
+//edit item by id with name, price
+app.put('/items/id', async (req, res) => {
+
+});
+
+//delete item by id
+app.delete('/items/id', async (req, res) => {
+
+});
+
+
+//CRUD functionalities for USERS
+//show all users
+app.get('/users', async (req, res) => {
+
+});
+
+//add new user with username
+app.post('/users', async (req, res) => {
+
+});
+
+//edit user by id with username
+app.put('/users/id', async (req, res) => {
+
+});
+
+//delete user by id
+app.delete('/users/id', async (req, res) => {
+
+});
+
+
+//CRUD functionalities for carts
+//show all carts
+app.get('/carts', async (req, res) => {
+
+});
+
+//add new cart with name
+app.post('/carts', async (req, res) => {
+
+});
+
+//delete cart by id
+app.delete('/carts/id', async (req, res) => {
+
+});
+
+//CRUD functionalities for item_in_cart
+//add item to cart with cart_id, item_id
+app.post('/shopping', async (req, res) => {
+
+});
+
+//delete item from cart by item_id with cart_id
+app.delete('/carts/itemid', async (req, res) => {
+
+});
+
+//clear all items in cart by cart_id
+app.delete('/carts/cartid', async (req, res) => {
 
 });
 
